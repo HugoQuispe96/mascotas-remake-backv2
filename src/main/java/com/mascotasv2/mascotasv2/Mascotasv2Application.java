15a4112940fe864bb5f03cf3619702a123cbc6ae
BrotherHugo <hugo.quispe96@outlook.es>
2021-07-10 01:28:38 -0400

    first commit

---
+++ src/main/java/com/mascotasv2/mascotasv2/Mascotasv2Application.java
@@ -0,0 +1,13 @@
+package com.mascotasv2.mascotasv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mascotasv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mascotasv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mascotasv2/mascotasv2/Mascotasv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mascotasv2.mascotasv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mascotasv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
