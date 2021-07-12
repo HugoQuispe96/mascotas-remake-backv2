4b1c5f36a9f107377c88b7ec02d53410864918b7
BrotherHugo <hugo.quispe96@outlook.es>
2021-07-12 00:13:57 -0400

    25

--- src/main/java/com/mascotasv3/mascotasv3/Mascotasv3Application.java
+++ src/main/java/com/mascotasv3/mascotasv3/Mascotasv3Application.java
@@ -1,13 +1,13 @@
-package com.mascotasv2.mascotasv2;
+package com.mascotasv3.mascotasv3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Mascotasv2Application {
+public class Mascotasv3Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Mascotasv2Application.class, args);
+		SpringApplication.run(Mascotasv3Application.class, args);
 	}
 
 }
